0cedb70fb528bc942d7dec4ebe1024e47e2fdb49
mvmangescu <mangy1984@yahoo.com>
2026-05-24 08:51:52 +0300

    refactoring

--- src/main/java/com/rulex/mapper/RuleMapper.java
+++ src/main/java/com/rulex/mapper/RuleMapper.java
@@ -2,11 +2,7 @@ package com.rulex.mapper;
 
 import com.rulex.dto.RuleResponse;
 import com.rulex.entity.RuleEntity;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface RuleMapper {
@@ -14,14 +10,10 @@ public interface RuleMapper {
     RuleResponse toRuleResponse(RuleEntity entity);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     RuleEntity toEntity(RuleResponse dto);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "name", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     void applyNonNull(RuleResponse dto, @MappingTarget RuleEntity entity);
 }
