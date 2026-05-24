df689d0b34f31819748dc13e7c4335e830128c27
mvmangescu <mangy1984@yahoo.com>
2026-05-24 11:06:45 +0300

    refactoring

--- src/main/java/com/rulex/repository/RuleRepository.java
+++ src/main/java/com/rulex/repository/RuleRepository.java
@@ -3,9 +3,5 @@ package com.rulex.repository;
 import com.rulex.entity.RuleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RuleRepository extends JpaRepository<RuleEntity, Long> {
-
-    Optional<RuleEntity> findByName(String name);
 }
